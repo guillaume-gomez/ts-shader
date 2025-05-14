a695acad0f0a6a8abe875513f9e487558a37822e
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-05-14 20:15:12 +0200

    remove default logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import ThreeJSRendering from "./Components/ThreeJS/ThreeJSRendering";
 import InputFileWithPreview from "./Components/InputFileWithPreview";
 import Toggle from "./Components/Toggle";
@@ -28,15 +26,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Tilt shift shader</h1>
       <InputFileWithPreview
           onChange={onChange}
           imageBase64={imageBase64}
