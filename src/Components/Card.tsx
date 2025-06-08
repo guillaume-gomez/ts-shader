14b8a3783ad1947071f1c1ecb3cfe9b9c3364c9a
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-06-08 15:02:07 +0200

    update design

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -9,10 +9,10 @@ interface CardProps {
 function Card({title, children} : CardProps) {
   return (
     <div className="card bg-base-200">
-      <div className="card-title">
+      <div className="card-title text-2xl">
         {title}
       </div>
-      <div className="card-body flex items-center gap-2">
+      <div className="card-body flex items-center gap-2 bg-base-300 rounded border border-secondary">
         {children}
       </div>
     </div>
